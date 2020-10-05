3ac54c2ce0032929a5850413e9fe47cdefa46e60
chen666250 <664635644@qq.com>
2020-10-05 09:08:12 +0800

    finished HE

---
+++ CIS2/HistogramEqualiztion/he2.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 08:32:14 2020
+
+@author: chen
+"""
+
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+#
+url="nightview.png"
+#url="livingroom.png"
+
+url="nocat.jpg"
+img=np.array(Image.open(url).convert('L'))
+
+
+MN=img.shape[0]*img.shape[1]
+
+ifla=img.flatten()
+cot=np.bincount(ifla,minlength=256)
+nk=cot/(MN)#512*512
+
+sk=np.zeros(256)
+for k in range(0,256):
+    sk[k]=255*nk[0:k].sum()
+    print("k is ",k,"sk  is",sk[k] )
+
+img1=img.copy()
+for pix in range(0,256):
+    img1[img==pix]=sk[pix]
+    
+#img1=sk[img]
+Image.fromarray(img1).show()
